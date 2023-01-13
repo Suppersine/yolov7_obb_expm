100ec169e9fbd039ac935fa928c6b0d93aa85af7
Supasin Wuthikulphakdi <49264553+Suppersine@users.noreply.github.com>
2023-01-13 10:18:29 +0800

    setlogging revised

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ from utils.autoanchor import check_anchors
 from utils.datasets import create_dataloader
 from utils.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
     fitness, strip_optimizer, get_latest_run, check_dataset, check_file, check_git_status, check_img_size, \
-    check_requirements, print_mutation, set_logging, one_cycle, colorstr
+    check_requirements, print_mutation, set_logging2, one_cycle, colorstr
 # from utils.downloads import attempt_download
 from utils.google_utils import attempt_download
 from utils.loss import ComputeLoss
@@ -578,7 +578,7 @@ if __name__ == '__main__':
     # Set DDP variables
     opt.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
     opt.global_rank = int(os.environ['RANK']) if 'RANK' in os.environ else -1
-    set_logging(opt.global_rank)
+    set_logging2(opt.global_rank)
     #if opt.global_rank in [-1, 0]:
     #    check_git_status()
     #    check_requirements()
