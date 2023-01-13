0c37f880308b06e51b60719431332fe889415cb4
Supasin Wuthikulphakdi <49264553+Suppersine@users.noreply.github.com>
2023-01-13 16:09:16 +0800

    Reverted to test.test
    Reverted to test.test to test out the Lee Gwang's test module

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.optim import SGD, Adam, lr_scheduler
 from tqdm import tqdm
 
-# import test  # import test.py to get mAP after each epoch
+import test  # import test.py to get mAP after each epoch
 from models.experimental import attempt_load
 from models.yolo import Model
 from utils.autoanchor import check_anchors
@@ -437,7 +437,7 @@ def train(hyp, opt, device, tb_writer=None):
             final_epoch = epoch + 1 == epochs
             if not opt.notest or final_epoch:  # Calculate mAP
                 wandb_logger.current_epoch = epoch + 1
-                results, maps, times = val.run(data_dict,
+                results, maps, times = test.test(data_dict,
                                                  batch_size=batch_size * 2,
                                                  imgsz=imgsz_test,
                                                  model=ema.ema,
@@ -517,7 +517,7 @@ def train(hyp, opt, device, tb_writer=None):
         logger.info('%g epochs completed in %.3f hours.\n' % (epoch - start_epoch + 1, (time.time() - t0) / 3600))
         if opt.data.endswith('coco.yaml') and nc == 80:  # if COCO
             for m in (last, best) if best.exists() else (last):  # speed, mAP tests
-                results, _, _ = val.run(opt.data,
+                results, _, _ = test.test(opt.data,
                                           batch_size=batch_size * 2,
                                           imgsz=imgsz_test,
                                           conf_thres=0.001,
